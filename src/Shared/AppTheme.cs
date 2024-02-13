5be0822315899b2cce6dff21a10bb5eac4e11fb5
SathiyaramanM <Sathiyaraman2003@gmail.com>
2024-02-13 23:47:10 +0530

    New Improved UI Color Theme

--- src/Shared/AppTheme.cs
+++ src/Shared/AppTheme.cs
@@ -4,22 +4,34 @@ namespace SmartLocate.Desktop.Admin.Shared;
 
 public static class AppTheme
 {
-    public static readonly MudTheme Default = new MudTheme()
+    public static readonly MudTheme Default = new()
     {
         Palette = new PaletteLight()
         {
             Primary = "#6750A4",
-            PrimaryLighten = "#EADDFF",
             PrimaryDarken = "#21005D",
-            Secondary = "#625B71",
-            SecondaryLighten = "#E8DEF8",
-            SecondaryDarken = "#1D192B"
-        },
-        PaletteDark = new PaletteDark()
-        {
-            Primary = "#D0BCFF",
-            PrimaryLighten = "#4F378B",
-            PrimaryDarken ="#EADDFF",
+            PrimaryLighten = "#EADDFF",
+            Black = "#222222",
+            Background = "#000000",
+            BackgroundGrey = "#232323",
+            Surface = "#272727",
+            DrawerBackground = "#2f2f2f",
+            DrawerText = "rgba(255,255,255, 0.50)",
+            DrawerIcon = "rgba(255,255,255, 0.50)",
+            AppbarBackground = "#6750A4",
+            AppbarText = "rgba(255,255,255, 0.70)",
+            TextPrimary = "rgba(255,255,255, 0.50)",
+            TextSecondary = "rgba(255,255,255, 0.70)",
+            ActionDefault = "#adadad",
+            ActionDisabled = "rgba(255,255,255, 0.26)",
+            ActionDisabledBackground = "rgba(255,255,255, 0.12)",
+            Divider = "rgba(255,255,255, 0.12)",
+            DividerLight = "rgba(255,255,255, 0.06)",
+            TableLines = "rgba(255,255,255, 0.12)",
+            LinesDefault = "rgba(255,255,255, 0.12)",
+            LinesInputs = "rgba(255,255,255, 0.3)",
+            TextDisabled = "rgba(255,255,255, 0.2)",
+            Tertiary = "#2f2f2f"
         },
         LayoutProperties = new LayoutProperties()
         {
